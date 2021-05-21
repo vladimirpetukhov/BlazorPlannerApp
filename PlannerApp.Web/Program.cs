0e2b16f943ff9e5b7f7e511636ad3cb8ddf33a1b
Vladimir Petukhov <VladimirPetukhov@students.softuni.bg>
2021-05-21 23:48:27 +0300

    ADD: login component

--- PlannerApp.Web/Program.cs
+++ PlannerApp.Web/Program.cs
@@ -1,17 +1,15 @@
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
-using PlannerApp.Shared;
-using PlannerApp.Shared.Services;
-
 namespace PlannerApp.Web
 {
+    #region usings
+    using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+    using Microsoft.Extensions.DependencyInjection;
+    using System;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using PlannerApp.Shared.Services;
+    using Blazored.LocalStorage;
+    #endregion
+
     public class Program
     {
         private const string URL = "https://plannerappserver20200228091432.azurewebsites.net";
@@ -28,6 +26,9 @@ namespace PlannerApp.Web
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddBlazoredLocalStorage();
+            builder.Services.AddOptions();
+            builder.Services.AddAuthorizationCore();
 
             await builder.Build().RunAsync();
         }
