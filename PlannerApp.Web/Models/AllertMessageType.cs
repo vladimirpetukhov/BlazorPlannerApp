626b3e9c5b6df7929d1ced3ff19124ad055d97b6
Vladimir Petukhov <VladimirPetukhov@students.softuni.bg>
2021-05-21 23:02:53 +0300

    ADD: allert messages

---
+++ PlannerApp.Web/Models/AllertMessageType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlannerApp.Web.Models
+{
+    public enum AlertMessageType
+    {
+        Error, Warning, Success
+    }
+}
